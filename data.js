d45c47c110c3b975851864c7d5ae5ee4ba104d2a
JonathanGranados26 <111204711+JonathanGranados26@users.noreply.github.com>
2022-09-02 16:42:05 -1200

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "libro ",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "pluma ",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "lapicero ",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "cuaderno ",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+];
